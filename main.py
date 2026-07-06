a07a988846dadbcee84dd684c37e7b98163b2c52
Abeeb <Olatunjia@thepaycocld.onmicrosoft.com>
2026-07-06 09:32:49 +0100

    read/write to json file

--- main.py
+++ main.py
@@ -1,35 +1,172 @@
-
 # ###########################################################
-# Data Analysis
+# GUI
 
-# CSV File Processing
+# import tkinter as tk
+from tkinter import  *
+import json
 
-# import  csv
-import pandas
-from numpy.ma.extras import average
+#Creating a new window and configurations
+window = Tk()
+window.title("Widget Examples")
+window.minsize(width=500, height=500)
 
-# with open("./Resources/weather.csv", mode="r") as data_file:
-    # data = data_file.readlines() # direct line-by-line read
-    # data = csv.reader(data_file) # inbuilt csv package
+#Labels
+label = Label(text="This is old text")
+label.config(text="This is new text")
+label.pack() # asides .pack(), .place(0,0) - coordinate, or .grid(column=0, row= 2) - grid system can also be used to add components to the window
 
-data = pandas.read_csv("./Resources/weather.csv") # returns a dataframe (table)
-temperatures = data["temp"] # returns a series (column)
-conditions = data.condition # returns a series (column)
-# print(f"average: {average(temperatures)}")
-# print(f"mean: {temperatures.mean()}")
-# print(f"max: {temperatures.max()}")
-# print(f"min: {temperatures.min()}")
-max_temp_row = data[data.temp == temperatures.max()]
-print(max_temp_row)
+#Buttons
+def action():
+    print("Do something")
+    website = website_entry.get()
+    email = email_entry.get()
+    new_data = {
+        website: {
+            "email": email
+        }
+    }
 
-# create a dataframe from dictionary
-dic = {
-    "name" : ["Abeeb", "Ray", "Alm"],
-    "age" : [20, 10, 6]
-}
+    # open file to read json data and update it
+    with open("test.json", "r") as json_file_read:
+        contents = json.load(json_file_read)
+        contents.update(new_data)
 
-data_frame = pandas.DataFrame(dic)
+    # open file to write the updated data
+    with open("test.json", "w") as json_file_write:
+        json.dump(contents, json_file_write, indent=4)
+
+    website_entry.delete(0, END)
+    email_entry.delete(0, END)
+
+#calls action() when pressed
+button = Button(text="Click Me", command=action)
+button.pack()
+
+#Entries
+website_entry = Entry(width=30)
+#Add some text to begin with
+website_entry.insert(END, string="Amazon")
+#Gets text in entry
+print(website_entry.get())
+website_entry.pack()
+
+email_entry = Entry(width=30)
+#Add some text to begin with
+email_entry.insert(END, string="example@amazon.com")
+email_entry.pack()
+
+#Text
+text = Text(height=5, width=30)
+#Puts cursor in textbox.
+text.focus()
+#Adds some text to begin with.
+text.insert(END, "Example of multi-line text entry.")
+#Get's current value in textbox at line 1, character 0
+print(text.get("1.0", END))
+text.pack()
+
+#Spinbox
+def spinbox_used():
+    #gets the current value in spinbox.
+    print(spinbox.get())
+spinbox = Spinbox(from_=0, to=10, width=5, command=spinbox_used)
+spinbox.pack()
+
+#Scale
+#Called with current scale value.
+def scale_used(value):
+    print(value)
+scale = Scale(from_=0, to=100, command=scale_used)
+scale.pack()
+
+#Checkbutton
+def checkbutton_used():
+    #Prints 1 if On button checked, otherwise 0.
+    print(checked_state.get())
+#variable to hold on to checked state, 0 is off, 1 is on.
+checked_state = IntVar()
+checkbutton = Checkbutton(text="Is On?", variable=checked_state, command=checkbutton_used)
+checked_state.get()
+checkbutton.pack()
+
+#Radiobutton
+def radio_used():
+    print(radio_state.get())
+#Variable to hold on to which radio button value is checked.
+radio_state = IntVar()
+radiobutton1 = Radiobutton(text="Option1", value=1, variable=radio_state, command=radio_used)
+radiobutton2 = Radiobutton(text="Option2", value=2, variable=radio_state, command=radio_used)
+radiobutton1.pack()
+radiobutton2.pack()
+
+
+#Listbox
+def listbox_used(event):
+    # Gets current selection from listbox
+    print(listbox.get(listbox.curselection()))
+
+listbox = Listbox(height=4)
+fruits = ["Apple", "Pear", "Orange", "Banana"]
+for item in fruits:
+    listbox.insert(fruits.index(item), item)
+listbox.bind("<<ListboxSelect>>", listbox_used)
+listbox.pack()
+
+window.mainloop() # keep window open until closed
+
+
+
+# # ###########################################################
+# # Data Analysis
+#
+# # CSV File Processing
+#
+# # import  csv
+# import pandas
+# from numpy.ma.extras import average
+#
+# # with open("./Resources/weather.csv", mode="r") as data_file:
+#     # data = data_file.readlines() # direct line-by-line read
+#     # data = csv.reader(data_file) # inbuilt csv package
+#
+# data = pandas.read_csv("./Resources/weather.csv") # returns a dataframe (table)
+# temperatures = data["temp"] # returns a series (column)
+# conditions = data.condition # returns a series (column)
+# # print(f"average: {average(temperatures)}")
+# # print(f"mean: {temperatures.mean()}")
+# # print(f"max: {temperatures.max()}")
+# # print(f"min: {temperatures.min()}")
+# max_temp_row = data[data.temp == temperatures.max()]
+# # print(max_temp_row)
+#
+# # create a dataframe from dictionary
+# dic = {
+#     "name" : ["Abeeb", "Ray", "Alm"],
+#     "age" : [20, 10, 6]
+# }
+#
+# # data_frame = pandas.DataFrame(dic)
+# # data_frame.to_csv("test.csv") # writes data frame to .csv file
+#
+# squirrel_data = pandas.read_csv("./Resources/2018_Central_Park_Squirrel_Census_-_Squirrel_Data.csv")
+# gray_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Gray"])
+# cinnamon_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Cinnamon"])
+# black_count = len(squirrel_data[squirrel_data["Primary Fur Color"] == "Black"])
+#
+# s_dict = {
+#     "color" : ["Gray", "Cinnamon", "Black"],
+#     "count" : [gray_count, cinnamon_count, black_count]
+# }
+#
+# data_frame = pandas.DataFrame(s_dict)
 # data_frame.to_csv("test.csv") # writes data frame to .csv file
+#
+# with open("file1.txt") as f1:
+#     with open("file2.txt") as f2:
+#         a = f1.readlines()
+#         b = f2.readlines()
+#         c = [int(n) for n in a if n not in b]
+
 
 
 # GAMES
